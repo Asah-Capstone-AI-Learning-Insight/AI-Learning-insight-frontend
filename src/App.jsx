c2569b8b5855f904093fbdc974fa59b1a2f8ba93
uky-syauki <achmad.uky@gmail.com>
2025-11-21 12:16:52 +0800

    Menambahkan halaman login

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
+import LoginPage from './pages/LoginPage';
+import ProtectedRoute from './routes/ProtectedRoute';
 
 function App() {
   return (
-    <>
-      <Dashboard />
-    </>
+    <Routes>
+      {/* Protected Route */}
+      <Route
+        path="/"
+        element={
+          // <ProtectedRoute>
+            <Dashboard />
+          // </ProtectedRoute>
+        }
+      />
+
+      {/* Public Route */}
+      <Route path="/login" element={<LoginPage />} />
+    </Routes>
   );
 }
 
